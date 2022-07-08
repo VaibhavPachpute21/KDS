f0c9103494e3edb8227c5e537540732e1f9dde6f
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-07-08 19:10:02 +0530

    done for the day!

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,32 +6,42 @@ export default function Home() {
 
     return (
         <>
-            <div>
-            <div class="banner">
-                <img class='bg1' src='./images/bk1.png' />
-                <img class='bg2' src='./images/circle.png' />
-                <img class='bg3' src='./images/Image_1.png' />
-                <div class='content overlay'>
-                    <strong >Welcome to Kushel Digi</strong>
-                    <h2>IT Consulting Services For <br /> Your Buisiness</h2>
-                    <p>We are leading technology solutions providing company all over the world doing over 40 years.</p>
-                    <button class='btn btn-outline-info'>Read more</button>
+            <div class="home bg-dark">
+                <div class="banner">
+                    <img class='bg1' src='./images/bk1.png' />
+                    <img class='bg2' src='./images/circle.png' />
+                    <img class='bg3' src='./images/Image_1.png' />
+                    <div class='content overlay'>
+                        <strong >Welcome to Kushel Digi</strong>
+                        <h2>IT Consulting Services For <br /> Your Buisiness</h2>
+                        <p>We are leading technology solutions providing company all over the world doing over 40 years.</p>
+                        <button class='btn btn-outline-info'>Read more</button>
+                    </div>
                 </div>
-            </div>
 
-            <div class="container">
-                <div class="">
-                    <img src='./images/23.png'></img>
-                    <strong>Your digital partner to create inspirational design and robust solution for Web, Mobile & Cloud.</strong>
-                    <p>The top-notch and preeminent Web and Mobile App Development Company of India with its global presence in USA, MindInventory is first-rate choice of the clients worldwide. With sheer customer satisfaction in mind, we are profoundly dedicated to developing highly intriguing apps that strictly meet the business requirements and catering a wide spectrum of projects.
-                    </p>
+                <div class="whoAreWe pb-5">
+                    <div class="container">
+                        <div class="row">
+                            <div className='col-md-6'>
+                                <img src='./images/23.png' />
+                            </div>
+                            <div className='col-md-6'>
+                                <strong>Who we are</strong>
+                                <h2>Your digital partner to create inspirational design and robust solution for Web, Mobile & Cloud.</h2>
+                                <p>The top-notch and preeminent Web and Mobile App Development Company of India with its global presence in USA, MindInventory is first-rate choice of the clients worldwide. With sheer customer satisfaction in mind, we are profoundly dedicated to developing highly intriguing apps that strictly meet the business requirements and catering a wide spectrum of projects.
+                                </p>
+                                <button class='btn btn-outline-info'>About us</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
+                
 
 
             </div>
 
-            
+
         </>
     )
 }
